d78011760b550eee5a09c3628132bcbe084674f0
josephmchristy <josephmchristy@gmail.com>
2022-02-20 17:29:20 -0800

    Added journal entries screen.

---
+++ lib/screens/journal_entries.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class JournalEntriesScreen extends StatelessWidget {
+
+  static const routeKey = 'journal_entries';
+
+  JournalEntriesScreen({ Key? key }) : super(key: key);
+
+  final items = List<Map>.generate(1000, (i) {
+    return {
+      'title': 'Journal Entry $i',
+      'subtitle': 'Subtitle text for $i',
+    };
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Journal Entries'),
+      ),
+      body: ListView.builder(itemBuilder: (context, index){
+        return ListTile(
+          leading: FlutterLogo(),
+          trailing: Icon(Icons.more_horiz),
+          title: Text('Journal Entry ${items[index]['title']}'),
+          subtitle: Text('Example ${items[index]['subtitle']}'),
+        );
+      })
+      // body: ListView(
+      //   children: [
+      //     ListTile(
+      //       leading: FlutterLogo(),
+      //       trailing: Icon(Icons.more_horiz),
+      //       title: Text('Journal Entry N'),
+      //       subtitle: Text('Example subtitle'),
+      //     )
+      //   ],
+      // )
+    );
+  }
+}
